b4d1261f5963874a0df4678974dd97a443de9161
apoorv595 <94398835+apoorv595@users.noreply.github.com>
2024-06-15 15:31:19 +0530

    Add files via upload

---
+++ Connect4/src/main/java/com/internshala/connectfour/connect4/Main.java
@@ -0,0 +1,105 @@
+package com.internshala.connectfour.connect4;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private Controller controller;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+        GridPane rootGridPane = loader.load();
+
+        controller = loader.getController();
+        controller.createPlayground();
+
+        MenuBar menuBar = createMenu();
+        // Makes the width same as width of Primary Stage
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());  // Binds the width property of primary Stage to menuBar
+
+        Pane menuPane = (Pane) rootGridPane.getChildren().get(0);
+        menuPane.getChildren().add(menuBar);
+
+        Scene scene  = new Scene(rootGridPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Connect Four");
+        primaryStage.setResizable(false);     // Restricts to resize
+        primaryStage.show();
+
+    }
+
+    private MenuBar createMenu() {
+        // File menu
+        Menu fileMenu = new Menu("File");
+
+        MenuItem newGame = new MenuItem("New Game");
+        newGame.setOnAction(event -> controller.resetGame());
+
+        MenuItem resetGame = new MenuItem("Reset Game");
+        resetGame.setOnAction(event -> controller.resetGame());
+
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+
+        MenuItem exitGame = new MenuItem("Exit Game");
+        exitGame.setOnAction(event -> exitGame());
+
+        fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
+
+        //Help Menu
+        Menu helpMenu = new Menu("Help");
+        MenuItem aboutGame = new MenuItem("About Game");
+        aboutGame.setOnAction(event -> aboutConnect4());
+        
+        SeparatorMenuItem separatorMenuItem1 = new SeparatorMenuItem();
+        MenuItem aboutMe = new MenuItem("About Me");
+        aboutMe.setOnAction(event -> aboutMe());
+
+        helpMenu.getItems().addAll(aboutGame, separatorMenuItem1, aboutMe);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+        return menuBar;
+
+    }
+
+    private void aboutMe() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About the Developer");
+        alert.setHeaderText("Ashutosh");
+        alert.setContentText("fs");
+        alert.show();
+    }
+
+    private void aboutConnect4() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About Connect Fout");
+        alert.setHeaderText("How to Play");
+        alert.setContentText("Connect Four is a two-player connection game in which the players first choose a color and then take turns dropping colored discs\n" +
+                "from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the next available space\n" +
+                "within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs.\n" +
+                "Connect Four is a solved game. The first player can always win by playing the right moves.");
+        alert.show();
+    }
+
+    private void exitGame() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
